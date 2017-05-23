ad70b92c989b0686f06e79d68d5d4a96bb4a0664
ioc0 <ioc0kernel@gmail.com>
2017-05-23 21:29:20 +0300

    Something added

--- RGUPST/Form1.cs
+++ RGUPST/Form1.cs
@@ -1,15 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Xml;
-using System.Text;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace RGUPST
 {
@@ -22,36 +13,30 @@ namespace RGUPST
 
         private void tsmExit_Click(object sender, EventArgs e)
         {
-            this.Close();
+            Close();
         }
 
         private void tsmOpen_Click(object sender, EventArgs e)
         {
-            OpenFileDialog dlg = new OpenFileDialog();
+            var dlg = new OpenFileDialog();
             dlg.Title = "Open XML Document";
             dlg.Filter = "XML Files (*.xml)|*.xml";
             dlg.FileName = Application.StartupPath + "\\..\\..\\example.xml";
             if (dlg.ShowDialog() == DialogResult.OK)
-            {
                 try
                 {
-                    //Just a good practice -- change the cursor to a 
-                    //wait cursor while the nodes populate
-                    this.Cursor = Cursors.WaitCursor;
-                    //First, we'll load the Xml document
-                    XmlDocument xDoc = new XmlDocument();
+                    Cursor = Cursors.WaitCursor;
+
+                    var xDoc = new XmlDocument();
                     xDoc.Load(dlg.FileName);
-                    //Now, clear out the treeview, 
-                    //and add the first (root) node
+
                     treeView1.Nodes.Clear();
                     treeView1.Nodes.Add(new
                         TreeNode(xDoc.DocumentElement.Name));
-                    TreeNode tNode = new TreeNode();
-                    tNode = (TreeNode) treeView1.Nodes[0];
-                    //We make a call to addTreeNode, 
-                    //where we'll add all of our nodes
+                    var tNode = new TreeNode();
+                    tNode = treeView1.Nodes[0];
                     addTreeNode(xDoc.DocumentElement, tNode);
-                    //Expand the treeview to show all nodes
+
                     treeView1.ExpandAll();
                 }
                 catch (XmlException xExc)
@@ -65,9 +50,8 @@ namespace RGUPST
                 }
                 finally
                 {
-                    this.Cursor = Cursors.Default; //Change the cursor back
+                    Cursor = Cursors.Default; //Change the cursor back
                 }
-            }
         }
 
         private void addTreeNode(XmlNode xmlNode, TreeNode treeNode)
@@ -78,7 +62,7 @@ namespace RGUPST
             if (xmlNode.HasChildNodes) //The current node has children
             {
                 xNodeList = xmlNode.ChildNodes;
-                for (int x = 0; x <= xNodeList.Count - 1; x++)
+                for (var x = 0; x <= xNodeList.Count - 1; x++)
                     //Loop through the child nodes
                 {
                     xNode = xmlNode.ChildNodes[x];
@@ -88,7 +72,9 @@ namespace RGUPST
                 }
             }
             else //No children, so add the outer xml (trimming off whitespace)
+            {
                 treeNode.Text = xmlNode.OuterXml.Trim();
+            }
         }
 
         private void actionOnClick(object sender, TreeNodeMouseClickEventArgs e)
@@ -103,28 +89,22 @@ namespace RGUPST
                 case "USA":
                     PdfShowDocument();
                     break;
-
             }
-            
-            
-            
         }
 
         private static void PdfShowDocument()
         {
-            PdfViewer pdfViewer = new PdfViewer();
+            var pdfViewer = new PdfViewer();
             pdfViewer.ShowMeSomething(@"C:\Test\card1.pdf.pdf");
             pdfViewer.Show();
         }
 
         private static void WebViewShow()
         {
-            Viewer view = new Viewer();
+            var view = new Viewer();
             view.ShowMeSuperDuper(
                 @"https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0");
             view.Show();
         }
     }
 }
-        
-
