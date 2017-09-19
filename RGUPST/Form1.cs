6ac61a6cf7f4b1a641e5093a3a40e8db88088923
ioc0 <ioc0kernel@gmail.com>
2017-09-19 06:17:34 +0300

    final version
    19.09.2017

--- RGUPST/Form1.cs
+++ RGUPST/Form1.cs
@@ -83,10 +83,10 @@ namespace RGUPST
             {
                 case "":
                     return;
-                case "1987":
+                case "Internet":
                     WebViewShow();
                     break;
-                case "USA":
+                case "Релейная защита, стр 8":
                     PdfShowDocument();
                     break;
             }
@@ -95,7 +95,7 @@ namespace RGUPST
         private static void PdfShowDocument()
         {
             var pdfViewer = new PdfViewer();
-            pdfViewer.ShowMeSomething(@"C:\Test\RZ.pdf",5);
+            pdfViewer.ShowMeSomething(@"C:\Test\RZ.pdf",8);
             pdfViewer.Show();
         }
 
@@ -103,7 +103,7 @@ namespace RGUPST
         {
             var view = new Viewer();
             view.ShowMeSuperDuper(
-                @"https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0");
+                @"https://ru.wikipedia.org/wiki/Релейная_защита_и_автоматика");
             view.Show();
         }
     }
